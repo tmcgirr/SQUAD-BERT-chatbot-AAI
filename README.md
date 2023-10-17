5accc7af5b3c852636c80919ebf24bc028e2ee73
Trevor McGirr <trevormcgirr@gmail.com>
2023-10-16 19:44:53 -0700

    Upload Chatbot

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
-# 📦 Streamlit App Starter Kit 
-```
-⬆️ (Replace above with your app's name)
-```
+# Chatbot using BERT and Streamlit
 
-Description of the app ...
+<!-- Description of the app ... -->
 
-## Demo App
+This chatbot was fine tuned using the BERT model and the SQuAD dataset. The dataset was tokenized and stored in a Pinecone index. The chatbot uses the Pinecone index to find the most similar question to the user's input and returns the answer to the question. The chatbot is deployed using Streamlit.
+
+## Quickstart
 
 [![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://app-starter-kit.streamlit.app/)
 
@@ -15,11 +14,12 @@ Description of the app ...
 
 ## Section Heading
 
-This is filler text, please replace this with text for this section.
+This is filler text, please replace this with a explanatory text about the section
 
 ## Further Reading
 
 This is filler text, please replace this with a explanatory text about further relevant resources for this repo
+
 - Resource 1
 - Resource 2
 - Resource 3
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,131 @@
+import os
 import streamlit as st
+import requests
+from dotenv import load_dotenv
 
-st.title('🎈 App Name')
+# LangChain
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Pinecone
 
-st.write('Hello world!')
+import openai
+
+
+# Pinecone
+import pinecone
+
+load_dotenv()
+
+PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY")
+PINECONE_INDEX_NAME = os.environ.get("PINECONE_INDEX_NAME")
+
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+openai.api_key = OPENAI_API_KEY
+
+pinecone.init(       
+ api_key=PINECONE_API_KEY,
+ environment=PINECONE_INDEX_NAME 
+)      
+index = pinecone.Index('chatbot')
+
+# OpenAI Embeddings
+embeddings = OpenAIEmbeddings()
+
+# Hugging Face Credentials
+hf_token = os.getenv('HF_TOKEN')
+
+API_URL = "https://api-inference.huggingface.co/models/tmcgirr/BERT-squad-chatbot-AAI"
+headers = {"Authorization":  f"Bearer {hf_token}"}
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
+# Check if Hugging Face credentials are provided
+if not hf_token:
+    st.error('HuggingFace Login credentials not provided!')
+    st.stop()
+
+# App title
+st.set_page_config(page_title="BERT Chatbot")
+
+# Define VectorStore
+vectorstore = Pinecone(index, embeddings.embed_query, "text")
+
+
+# Initialize conversation history if it doesn't exist
+if "conversation" not in st.session_state:
+    st.session_state.conversation = [{"role": "assistant", "content": "How may I help you?"}]
+
+# Display conversation history
+for message in st.session_state.conversation:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# Function for generating search report
+def generate_search_report(prompt: str, k: int):
+    xq = openai.Embedding.create(input=[prompt], engine="text-embedding-ada-002")['data'][0]['embedding']
+    docs = index.query([xq], top_k=k, include_metadata=True)
+    return docs
+
+# Function for generating LLM response
+def generate_response(prompt_input):
+    # Generate search based on question (and the last 500 characters of the conversation history)
+    conversation_history = [message['content'] for message in st.session_state.conversation]
+    print("\n\nStart of conversation history")
+    print(conversation_history)
+    print("End of conversation history\n\n")
+    
+    # Generate search based on question and the last 500 characters of the conversation history
+    search_context = " ".join(conversation_history[-500:]) + " " + prompt_input
+    print("\n\nStart of search context")
+    print(search_context)
+    print("End of search context\n\n")
+    
+    # generate search based on question
+    docs = generate_search_report(search_context, 5)
+    print("\n\nStart of docs")
+    print(docs)
+    print("End of docs\n\n")
+
+    # Extract the context from the output
+    context = " ".join([match['metadata']['text'] for match in docs['matches']])
+    print("\n\nStart of context")
+    print(context)
+    print("End of context\n\n")
+
+    payload = {
+        "inputs": {
+            "question": prompt_input,
+            "context": context
+        },
+    }
+    print("Question: ", prompt_input)
+    print("Context: ", context)
+
+    
+    output = query(payload)
+    print("\n\nStart of output")
+    print(output)
+    print("End of output\n\n")
+
+    # Check if 'answer' is in the output
+    if 'answer' in output:
+        return output['answer']
+    else:
+        # Handle error: 'answer' not in output
+        return "I'm sorry, I couldn't find an answer to your question."
+
+# User-provided prompt
+if prompt := st.chat_input():
+    st.session_state.conversation.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+
+# Generate a new response if last message is not from assistant
+if st.session_state.conversation[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = generate_response(prompt) 
+            st.write(response) 
+    message = {"role": "assistant", "content": response}
+    st.session_state.conversation.append(message)
